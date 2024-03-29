fd2bf95e4b56d6d073d9206be094c9909a7baa04
kame-sennin23 <dalsasso.gino@gmail.com>
2024-03-29 16:18:39 +0100

    CSS details views

--- view/detail/detailRealisateur.php
+++ view/detail/detailRealisateur.php
@@ -6,43 +6,43 @@
 $realisateur = $requeteRealisateur->fetch();
 $filmRealisateur = $requeteFilmRealisateur->fetchAll();?>
 
+
+<span id="add_btn_position">
+    <a class="add_btn" href="index.php?action=editRealisateur&id=<?= $realisateur["id_realisateur"] ?>">Editer realisateur</a>
+</span>
 <!-- Affichage des informations sur le réalisateur -->
-<div>
-    <img src="<?= $realisateur['photo']?>" alt="photo du réalisateur">
-    <p>Date de naissance: <?= $realisateur["dateNaissance"]?></p>
-    <p>Sexe: <?= $realisateur["sexe"]?></p>
-</div>
+<div class="wrapper">
+    <div class="detail">
+        <figure class="fade_card">
+            <img src="<?= $realisateur['photo']?>" alt="photo du réalisateur">
+        </figure>
+        <p>Date de naissance: <?= $realisateur["dateNaissance"]?></p>
+        <p>Sexe: <?= $realisateur["sexe"]?></p>
+        
+    </div>
 
-<!-- tableau affichant les films réalisés par ce réalisateur -->
-<table>
-    <thead>
-        <tr>
-            <th>titre</th>
-            <th>parution</th>
-            <th>id film</th>
-            <th>photo</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-            foreach($filmRealisateur as $film) { ?>
-                <tr>
-                    <td><?= $film["titre"] ?></td>
-                    <td><?= $film["parution"] ?></td>
-                    <td><?= $film["id_film"] ?></td>
-                    <td>
+    <!-- liste des films réalisés par le réalisateur -->
+        <h3>Films réalisés:</h3>
+        <div class="cards_list">
+            <?php
+                foreach($filmRealisateur as $film) { ?>
+                    <div class="card_item">
                         <a href="index.php?action=detailFilm&id=<?= $film["id_film"] ?>">
-                        <img src="<?= $film['affiche']?>" alt="affiche du film"></a>
-                    </td>
-                </tr>
-            <?php } ?>
-    </tbody>
-</table>
+                            <figure class="fade_card">
+                                <img src="<?= $film['affiche']?>" alt="affiche du film">    
+                            </figure>
+                        </a>
+                        <p><?= $film["titre"] ?></p>
+                        <p><?= $film["parution"] ?></p>
+                    </div>
 
+                <?php } ?>
+        </div>
+    </div>
 <?php
 
 // Définition des titres pour la vue
-$titre = "Détail acteurs";
+$titre = "Détail réalisateur";
 $titre_secondaire = $realisateur["nomRealisateur"]; //titre variable en fonction de l'acteur
 
 $contenu = ob_get_clean();
